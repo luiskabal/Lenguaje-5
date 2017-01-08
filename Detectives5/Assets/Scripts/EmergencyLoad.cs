afb69d49e1bffd13fa888e5cd3267d59d6024c9b
El Diablo <flagos@netcore.cl>
2017-01-07 21:13:13 -0300

    Avance selección mundos

---
+++ Detectives5/Assets/Scripts/EmergencyLoad.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmergencyLoad : MonoBehaviour
+{
+    public Loading loadingCanvas;
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void LoadWorldSelectScene(string name)
+    {
+        loadingCanvas.DoLoadLevel(name);
+    }
+
+
+}
