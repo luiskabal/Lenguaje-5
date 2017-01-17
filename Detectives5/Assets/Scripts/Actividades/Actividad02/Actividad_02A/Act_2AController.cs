46674f3a6fa7597a2ab6f36ea27d8c5d6914092d
luiskabals <luiskabals@gmail.com>
2017-01-17 17:01:46 -0300

    2a

--- Detectives5/Assets/Scripts/Actividades/Actividad02/Actividad_02A/Act_2AController.cs
+++ Detectives5/Assets/Scripts/Actividades/Actividad02/Actividad_02A/Act_2AController.cs
@@ -5,18 +5,19 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class Act_2AController : MonoBehaviour {
-
+    BigBoss bb;
     public string[] Oraciones;
     public GameObject[] Objetos;
     public GameObject Preguntas;
     public Text textoPregunta;
-
+    public GameObject Luz;
 
 	void Start () {
-		
+        bb = GameObject.FindWithTag("Scripts").GetComponent<BigBoss>();
+        Invoke("apagarLuz", 12f);
 	}
-    public void verificarSeleccion(GameObject o) {
-        Debug.Log("SE TOCA");
+    public void objetoSeleccionado(GameObject o) {
+        o.GetComponent<Button>().interactable = false;
         for (int i=0;i<Objetos.Length;i++) {
             if (o==Objetos[i]) {
                 textoPregunta.text = Oraciones[i];
@@ -26,12 +27,47 @@ public class Act_2AController : MonoBehaviour {
         }
 
     }
+    void apagarLuz() {
+        Luz.SetActive(false);
+
+    }
     void abrirPregunta() {
+        Preguntas.SetActive(true);
         Preguntas.GetComponent<Animator>().SetTrigger("Preguntas_On");
     }
     void cerrarPregunta()
     {
+        
         Preguntas.GetComponent<Animator>().SetTrigger("Preguntas_Off");
+        Invoke("borrarPreguntas",3f);
+    }
+    void borrarPreguntas() {
+        Preguntas.SetActive(false);
+    }
+  public  void verificarRespuesta(GameObject o) {
+        if (o.tag == "Verdadero") {
+            ganar();
+        }else
+        {
+
+
+
+        }
+
+    }
+    void ganar()
+    {
+        if (!bb.Good())
+        {
+            if (bb.StarWon())
+            {
+                cerrarPregunta();
+            }
+        }
+    }
+    void perder()
+    {
+        bb.Bad();
     }
 
 
