66d2520a5d86bcd5d1b2565aa26ed2a89e4dc291
luiskabals <luiskabals@gmail.com>
2017-01-17 13:48:55 -0300

    2a

---
+++ Detectives5/Assets/Scripts/Actividades/Actividad02/Actividad_02A/Act_2AController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Act_2AController : MonoBehaviour {
+
+    public string[] Oraciones;
+    public GameObject[] Objetos;
+    public GameObject Preguntas;
+    public Text textoPregunta;
+
+
+	void Start () {
+		
+	}
+    public void verificarSeleccion(GameObject o) {
+        Debug.Log("SE TOCA");
+        for (int i=0;i<Objetos.Length;i++) {
+            if (o==Objetos[i]) {
+                textoPregunta.text = Oraciones[i];
+                abrirPregunta();
+            }
+
+        }
+
+    }
+    void abrirPregunta() {
+        Preguntas.GetComponent<Animator>().SetTrigger("Preguntas_On");
+    }
+    void cerrarPregunta()
+    {
+        Preguntas.GetComponent<Animator>().SetTrigger("Preguntas_Off");
+    }
+
+
+}
