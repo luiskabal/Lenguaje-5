99f5064f50450810178b080d1559ffed0f0aadbf
luiskabals <luiskabals@gmail.com>
2017-01-17 22:30:52 -0300

    2a lista

---
+++ Detectives5/Assets/Scripts/Actividades/Actividad02/Actividad_02A/GeneradorAlternativas.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneradorAlternativas : MonoBehaviour {
+    [Header("Alternativas ¿Qué?")]
+
+    public string[] QUE_alternativas0;
+    public string[] QUE_alternativas1;
+    public string[] QUE_alternativas2;
+    public string[] QUE_alternativas3;
+    public string[] QUE_alternativas4;
+    public string[] QUE_alternativas5;
+    public string[] QUE_alternativas6;
+    public string[] QUE_alternativas7;
+    public string[] QUE_alternativas8;
+    public string[] QUE_alternativas9;
+
+    [Header("Alternativas ¿Quién?")]
+
+    public string[] Quien_alternativas0;
+    public string[] Quien_alternativas1;
+    public string[] Quien_alternativas2;
+    public string[] Quien_alternativas3;
+    public string[] Quien_alternativas4;
+    public string[] Quien_alternativas5;
+    public string[] Quien_alternativas6;
+    public string[] Quien_alternativas7;
+    public string[] Quien_alternativas8;
+    public string[] Quien_alternativas9;
+
+    [Header("Alternativas ¿Para qué?")]
+
+    public string[] PARAQUE_alternativas0;
+    public string[] PARAQUE_alternativas1;
+    public string[] PARAQUE_alternativas2;
+    public string[] PARAQUE_alternativas3;
+    public string[] PARAQUE_alternativas4;
+    public string[] PARAQUE_alternativas5;
+    public string[] PARAQUE_alternativas6;
+    public string[] PARAQUE_alternativas7;
+    public string[] PARAQUE_alternativas8;
+    public string[] PARAQUE_alternativas9;
+    [Header("Alternativas ¿Para Quién?")]
+
+    public string[] PARAQUIEN_alternativas0;
+    public string[] PARAQUIEN_alternativas1;
+    public string[] PARAQUIEN_alternativas2;
+    public string[] PARAQUIEN_alternativas3;
+    public string[] PARAQUIEN_alternativas4;
+    public string[] PARAQUIEN_alternativas5;
+    public string[] PARAQUIEN_alternativas6;
+    public string[] PARAQUIEN_alternativas7;
+    public string[] PARAQUIEN_alternativas8;
+    public string[] PARAQUIEN_alternativas9;
+
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+
+}
